51b9c087fb7019967b202478e38aacbf8c699f8c
Nazarishak <kiasenkonazeek@gmail.com>
2025-06-11 23:55:09 +0300

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,1 +1,15 @@
-
+CREATE TABLE imdb_top_movies (
+    rank INTEGER,
+    name TEXT,
+    year INTEGER,
+    rating NUMERIC,
+    genre TEXT,
+    certificate TEXT,
+    run_time TEXT,
+    tagline TEXT,
+    budget BIGINT,
+    box_office BIGINT,
+    casts TEXT,
+    directors TEXT,
+    writers TEXT
+);
